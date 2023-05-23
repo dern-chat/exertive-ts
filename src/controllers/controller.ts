c72a3f56b25d6940f8d47c6a753ad8045363a9fd
mucahitkurtlar <mucahit@tutanota.com>
2023-05-23 13:47:06 +0300

    Lint source code
    - Lint source code files using ESLint and EditorConfig

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -6,78 +6,78 @@ import * as jwtService from '../services/jwt.service'
 
 
 export function indexController(req: Request, res: Response) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 }
 
 export function roomInfoController(room: Room) {
-  return (req: Request, res: Response) => {
-    console.log('room info requested')
+    return (req: Request, res: Response) => {
+        console.log('room info requested')
 
-    console.log('auth header: ', req.header('Authorization'))
-    const token = req.header('Authorization')?.replace('Bearer ', '')
-    if (!token) {
-      res.sendStatus(401)
-      return
+        console.log('auth header: ', req.header('Authorization'))
+        const token = req.header('Authorization')?.replace('Bearer ', '')
+        if (!token) {
+            res.sendStatus(401)
+            return
+        }
+
+        console.log('token: ', token)
+        const nickname = jwtService.getNicknameFromToken(token)
+
+        if (!jwtService.isValidToken(token)) {
+            res.sendStatus(401)
+            return
+        }
+
+        res.send({ roomName: room.name, users: room.users, msgs: room.messages, nickname: nickname })
     }
-
-    console.log('token: ', token)
-    const nickname = jwtService.getNicknameFromToken(token)
-
-    if (!jwtService.isValidToken(token)) {
-      res.sendStatus(401)
-      return
-    }
-
-    res.send({ roomName: room.name, users: room.users, msgs: room.messages, nickname: nickname })
-  }
 }
 
 export function joinRoomController(room: Room) {
-  return (req: Request, res: Response) => {
-    const nickname: string = req.body.nickname
-    const passphrase: string = req.body.passphrase
+    return (req: Request, res: Response) => {
+        const nickname: string = req.body.nickname
+        const passphrase: string = req.body.passphrase
 
-    if (passphrase !== room.passphrase) {
-      res.sendStatus(401)
-      return
+        if (passphrase !== room.passphrase) {
+            res.sendStatus(401)
+            return
+        }
+        const token = jwtService.generateToken(nickname)
+        console.log('token: ', token)
+
+        try {
+            room.addUser(nickname)
+        }
+        catch (e) {
+            console.log(e)
+            res.sendStatus(400)
+            return
+        }
+
+        console.log('user joined: ', nickname)
+
+        res.cookie('token', token, { httpOnly: true })
+        res.send({ roomName: room.name, users: room.users, token: token })
     }
-    const token = jwtService.generateToken(nickname)
-    console.log('token: ', token)
-
-    try {
-      room.addUser(nickname)
-    }
-    catch (e) {
-      console.log(e)
-      res.sendStatus(400)
-      return
-    }
-
-    console.log('user joined: ', nickname)
-
-    res.cookie('token', token, { httpOnly: true })
-    res.send({ roomName: room.name, users: room.users, token: token })
-  }
 }
 
 export function messageController(room: Room, io: Server) {
-  return (req: Request, res: Response) => {
-    const { author, content, time } = req.body
-    const message = new Message(author, content, time)
-    room.addMessage(message)
-    console.log('message received: ', message)
+    return (req: Request, res: Response) => {
+        const { author, content, time } = req.body
+        const message = new Message(author, content, time)
+        room.addMessage(message)
+        console.log('message received: ', message)
 
-    io.emit('broadcast-message', message)
-    console.log('message broadcasted: ', message)
+        io.emit('broadcast-message', message)
+        console.log('message broadcasted: ', message)
 
-    res.sendStatus(200)
-  }
+        res.sendStatus(200)
+    }
 }
 
 export function messagesController(room: Room) {
-  return (req: Request, res: Response) => {
-    console.log('messages requested')
+    return (req: Request, res: Response) => {
+        console.log('messages requested')
 
-    res.send(room.messages)
-  }
+        res.send(room.messages)
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,20 @@ dotenv.config()
 const app: Express = express()
 const http = createServer(app)
 const io = new Server(http, {
-  cors: {
-    origin: '*',
-  }
+    cors: {
+        origin: '*',
+    }
 })
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
 
-  next()
+    next()
 })
 
 const room: Room = new Room("Test Room", '1234')
@@ -36,12 +36,12 @@ app.post('/api/message', controllers.messageController(room, io))
 app.get('/api/messages', controllers.messagesController(room))
 
 io.on('connection', (socket) => {
-  socket.on('user-enter', (msg) => {
-    console.log(msg);
-    io.emit('broadcast-user-enter', `${msg}`)
-  })
+    socket.on('user-enter', (msg) => {
+        console.log(msg);
+        io.emit('broadcast-user-enter', `${msg}`)
+    })
 })
 
 http.listen(2323, () => {
-  console.log('listening on http://localhost:2323')
+    console.log('listening on http://localhost:2323')
 })
