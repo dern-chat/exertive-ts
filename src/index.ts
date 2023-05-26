bdd4739dd6d9b35763f2fac5ea9668fcfdc9b9d6
mucahitkurtlar <mucahit@tutanota.com>
2023-05-26 13:36:14 +0300

    Get room info from user
    - Get room name and passphrase from user using an interactive way

--- src/index.ts
+++ src/index.ts
@@ -5,43 +5,62 @@ import { createServer } from 'http'
 import Room from './types/room'
 import * as controllers from './controllers/controller'
 import * as dotenv from 'dotenv'
+import prompts from 'prompts'
 
-dotenv.config()
+(async () => {
+    dotenv.config();
 
-const app: Express = express()
-const http = createServer(app)
-const io = new Server(http, {
-    cors: {
-        origin: '*',
-    }
-})
+    const userInput = await prompts([
+        {
+            type: 'text',
+            name: 'roomName',
+            message: 'Enter room name',
+            initial: 'Chat Room',
+            validate: value => value.length < 3 ? `Room name must be at least 3 characters` : true
+        },
+        {
+            type: 'password',
+            name: 'roomPassword',
+            message: 'Enter room password',
+            validate: value => value.length < 8 ? `Room password must be at least 8 characters` : true
+        }
+    ])
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-
-    next()
-})
-
-const room: Room = new Room("Test Room", '1234')
-
-app.get('/', controllers.indexController)
-app.get('/api/room-info', controllers.roomInfoController(room))
-app.post('/api/join-room', controllers.joinRoomController(room, io))
-app.post('/api/message', controllers.messageController(room, io))
-app.get('/api/messages', controllers.messagesController(room))
-
-io.on('connection', (socket) => {
-    socket.on('user-enter', (msg) => {
-        console.log(msg);
-        io.emit('broadcast-user-enter', `${msg}`)
+    const app: Express = express()
+    const http = createServer(app)
+    const io = new Server(http, {
+        cors: {
+            origin: '*',
+        }
     })
-})
 
-http.listen(2323, () => {
-    console.log('listening on http://localhost:2323')
-})
+    app.use(bodyparser.json())
+    app.use(bodyparser.urlencoded({ extended: false }))
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
+        res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+        next()
+    })
+
+    const room: Room = new Room(userInput.roomName, userInput.roomPassword)
+
+    app.get('/', controllers.indexController)
+    app.get('/api/room-info', controllers.roomInfoController(room))
+    app.post('/api/join-room', controllers.joinRoomController(room, io))
+    app.post('/api/message', controllers.messageController(room, io))
+    app.get('/api/messages', controllers.messagesController(room))
+
+    io.on('connection', (socket) => {
+        socket.on('user-enter', (msg) => {
+            console.log(msg);
+            io.emit('broadcast-user-enter', `${msg}`)
+        })
+    })
+
+    http.listen(2323, () => {
+        console.log('listening on http://localhost:2323')
+    })
+})()
